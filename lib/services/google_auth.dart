abf3652f0d9ac2c482415dc6b646fa138bf1be93
Jan Kubowicz <j.kubowicz7@gmail.com>
2024-01-04 16:44:56 +0100

    WIP: adding users to database via GoogleAuth

--- lib/services/google_auth.dart
+++ lib/services/google_auth.dart
@@ -1,8 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:telephon_application/controllers/crud_services.dart';
 
 class GoogleAuth {
   signInWithGoogle( ) async {
+    final CrudServices _crudServices = CrudServices();
+
     final GoogleSignInAccount? gUser = await GoogleSignIn().signIn();
 
     final GoogleSignInAuthentication gAuthentication = await gUser!.authentication;
@@ -11,6 +14,9 @@ class GoogleAuth {
       accessToken: gAuthentication.accessToken,
       idToken: gAuthentication.idToken
     );
+
+    //String name = gUser.email.substring(0, gUser.email.indexOf('@'));
+    //_crudServices.addUser(gUser.email, name, gCredential.idToken.toString());
 
     return await FirebaseAuth.instance.signInWithCredential(gCredential);
   }
