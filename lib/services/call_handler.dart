7d77acbc06039ad1ae226bd1f35270aa48db489d
Jan Kubowicz <j.kubowicz7@gmail.com>
2024-01-04 15:34:21 +0100

    WIP: calling page

---
+++ lib/services/call_handler.dart
@@ -0,0 +1,21 @@
+class CallHandler {
+  String? id;
+  String channel;
+  String caller;
+  String called;
+  bool? active;
+  bool? accepted;
+  bool? rejected;
+  bool? connected;
+
+  CallHandler({
+    required this.id,
+    required this.channel,
+    required this.caller,
+    required this.called,
+    required this.active,
+    required this.accepted,
+    required this.rejected,
+    required this.connected,
+  });
+}
